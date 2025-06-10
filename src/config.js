95d0e28e0a76fb69b6c4d8e011c385f117fbeb8c
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2025-06-10 00:29:57 -0300

    Remove more unused stuff

--- src/config.js
+++ src/config.js
@@ -2,11 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { TOKEN, OSU_API_SECRET, OSU_API_CALLBACK_URL } = process.env;
-const OSU_API_CLIENT_ID = Number(process.env.OSU_API_CLIENT_ID);
+const { TOKEN } = process.env;
 
-if (!OSU_API_CLIENT_ID || !OSU_API_SECRET || !OSU_API_CALLBACK_URL) {
-    throw new Error("Missing environment variables");
+if (!TOKEN) {
+    throw new Error("Missing TOKEN variable in .env file");
 }
 
 const ESNUPICORE = {
@@ -24,9 +23,6 @@ const MECHANICS = {
 
 export const config = {
     TOKEN,
-    OSU_API_CLIENT_ID,
-    OSU_API_SECRET,
-    OSU_API_CALLBACK_URL,
     ESNUPICORE,
     MECHANICS
 };
